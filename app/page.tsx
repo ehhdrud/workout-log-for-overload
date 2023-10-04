0788443fa157d3abb31b3445d037f148de13053f
서동경 <ehhdrud@icloud.com>
2023-10-05 04:31:42 +0900

    design: [main] - CSS 리펙토링

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
                 <div>
                     <FontAwesomeIcon icon={faKey} fontSize="12px" color="#dd0" />
                 </div>
-                <span>&ldquo;ehhdrud&rdquo;</span>
+                <span>ehhdrud</span>
             </div>
         </main>
     );
